ad148d140d459b8d2b5b2b818f4778c77755135a
Nicolas Mussat <nicolas@diffuzed.com>
2026-03-03 13:46:46 +0100

    fix: Replace deprecated -depth with -maxdepth/-mindepth in find commands

--- bin/init.sh
+++ bin/init.sh
@@ -4,7 +4,7 @@ set -eu
 
 [[ ! -d "$HOME/.config" ]] && mkdir -p "$HOME/.config"
 
-for source in $(find config/ -type d -depth 1 | xargs realpath); do
+for source in $(find config/ -maxdepth 1 -mindepth 1 -type d | xargs realpath); do
     target="$HOME/.config/$(basename "$source")";
     test -L "$target" && mv "$target"{,.bak}; # symlink
     test -e "$target" && mv "$target"{,.bak}; # regular file or folder
@@ -12,7 +12,7 @@ for source in $(find config/ -type d -depth 1 | xargs realpath); do
 done;
 
 
-for source in $(find dotfiles/ -type f -depth 1 | xargs realpath); do
+for source in $(find dotfiles/ -maxdepth 1 -mindepth 1 -type f | xargs realpath); do
     target="$HOME/$(basename "$source")";
     test -L "$target" && mv "$target"{,.bak}; # symlink
     test -e "$target" && mv "$target"{,.bak}; # regular file or folder
