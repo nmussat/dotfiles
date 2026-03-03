530f386b935466ee167d8a8eab8d693c1fa337c1
Nicolas Mussat <nicolas@diffuzed.com>
2026-03-03 13:45:55 +0100

    Fix unquoted variables in init.sh to handle paths with spaces

--- bin/init.sh
+++ bin/init.sh
@@ -5,16 +5,16 @@ set -eu
 [[ ! -d "$HOME/.config" ]] && mkdir -p "$HOME/.config"
 
 for source in $(find config/ -type d -depth 1 | xargs realpath); do
-    target="$HOME/.config/$(basename $source)";
-    test -L $target && mv $target{,.bak}; # symlink
-    test -e $target && mv $target{,.bak}; # regular file or folder
-    ln -s $source $target;
+    target="$HOME/.config/$(basename "$source")";
+    test -L "$target" && mv "$target"{,.bak}; # symlink
+    test -e "$target" && mv "$target"{,.bak}; # regular file or folder
+    ln -s "$source" "$target";
 done;
 
 
 for source in $(find dotfiles/ -type f -depth 1 | xargs realpath); do
-    target="$HOME/$(basename $source)";
-    test -L $target && mv $target{,.bak}; # symlink
-    test -e $target && mv $target{,.bak}; # regular file or folder
-    ln -s $source $target;
+    target="$HOME/$(basename "$source")";
+    test -L "$target" && mv "$target"{,.bak}; # symlink
+    test -e "$target" && mv "$target"{,.bak}; # regular file or folder
+    ln -s "$source" "$target";
 done;
